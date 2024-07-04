e7f81e781146e13bb7179cec706f8bb86b81fade
SAOSTAR1501 <saostar1501@gmail.com>
2024-07-04 23:46:09 +0700

    create_user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,11 @@
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "jwt_node_react",
+});
+
 const handleHelloWorld = (req, res) => {
   const name = "Star";
   return res.render("home.ejs", { name });
@@ -7,4 +15,25 @@ const handleUserPage = (req, res) => {
   return res.render("user.ejs");
 };
 
-module.exports = { handleHelloWorld, handleUserPage };
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+
+  connection.query(
+    "INSERT INTO users (email, username, password) VALUES (?, ?, ?)",
+    [email, username, password],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  return res.send("Created User!");
+};
+
+module.exports = {
+  handleHelloWorld,
+  handleUserPage,
+  handleCreateNewUser,
+};
